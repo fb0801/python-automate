55355ed17a8c714a568e5d89f9b5f5e2d403b8a3
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-08-31 18:05:24 +0100

    Add files via upload

--- automate beginner/multiclipboard.py
+++ automate beginner/multiclipboard.py
@@ -2,4 +2,16 @@ import sys
 import clipboard
 import json
 
-clipboard.copy("abc")
+#clipboard.copy("abc")
+
+if len(sys.argv) == 2:
+    command =sys.argv[1]
+    print (command)
+
+    if command == "save":
+        print('save')
+    elif command == 'load':
+        print ('load')
+    else:
+        print('unknown command')
+    
