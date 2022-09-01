febf0535f48da489b7d1563190ed7bb32bd4a6c7
Farhan Bhatti <74499472+fb0801@users.noreply.github.com>
2022-09-01 19:10:40 +0100

    Add files via upload

--- automate beginner/multiclipboard.py
+++ automate beginner/multiclipboard.py
@@ -9,24 +9,24 @@ SAVED_DATA = "clipboard.json"
 
 
 #save items
-def save_items(filepath, data):
+def save_data(filepath, data):
     with open(filepath, 'w') as f:
         json.dump(data, f)
 
 #load items
-def load_items(filepath):
+def load_data(filepath):
     with open(filepath, 'r') as f:
         data = json.load(f)
         return data
 
 if len(sys.argv) == 2:
     command =sys.argv[1]
-    data = load_items(SAVED_DATA)
+    data = load_data(SAVED_DATA)
 
     if command == "save":
         key = input('Enter a key: ')
         data[key] = clipboard.paste()
-        save_items(,data)
+        save_data(SAVED_DATA, data)
     elif command == 'load':
         print ('load')
     else:
